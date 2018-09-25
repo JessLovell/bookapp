84eb47109292a6bc2391478c100f44a5827c7a20
George McCadden III <thethirdone.g@gmail.com>
2018-09-25 10:09:35 -0700

    Completed the implementation of feature 1

--- server.js
+++ server.js
@@ -1,1 +1,26 @@
 'use strict';
+
+// Application Dependencies
+// ExpressJS allows us to use the "app.get" syntax
+const express = require('express');
+
+// Superagent makes our proxied API requests
+const superagent = require('superagent');
+
+// Instantiate ExpressJS so we can utilize its methods
+const app = express();
+
+// Making sure the server knows which poty yo listen for requests on
+const PORT = process.env.PORT || 3000;
+
+// Application Middleware
+// If you have a public folder with some CSS files..
+app.use(express.static('./public'));
+app.use(express.urlencoded({extended: true}));
+
+// Setting the view engine for server-side templating
+app.set('view engine', 'ejs');
+
+app.get('/hello', (request, response) => response.render('pages/index'));
+
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
